7e0f55357db35b68b8cccc623f0b2756d4bdc3ac
Breno Mendes <b.mendesbernardo2020@gmail.com>
2024-04-19 13:54:30 -0300

    feat: Adição de função/arquivo para extração e validação de arquivos compactados

--- ingestao/ingestion.py
+++ ingestao/ingestion.py
@@ -5,16 +5,17 @@ import shutil
 
 sys.path.insert(0, os.getcwd())
 from utils.get_element_selenium import get_element_data
+from utils.extract_file import extract_file
 
-def move_file(downloads_dir, data_folder):
+def move_file(downloads_dir, folder_files_zip):
     search_pattern = os.path.join(downloads_dir, "HIST_PAINEL_COVIDBR_13abr2024.zip")
     matching_files = glob.glob(search_pattern)
 
     if matching_files:
-        if not os.path.exists(data_folder):
-            os.makedirs(data_folder)
+        if not os.path.exists(folder_files_zip):
+            os.makedirs(folder_files_zip)
 
-        shutil.move(matching_files[0], data_folder)
+        shutil.move(matching_files[0], folder_files_zip)
 
     else:
         print("Arquivo não encontrado na pasta de Downloads.")
@@ -26,8 +27,12 @@ if __name__ == '__main__':
     user_folder = os.path.expanduser('~')
     downloads_dir = os.path.join(user_folder, 'Downloads')
 
+    folder_files_zip = os.path.join(data_folder, 'zip_files')
+    folder_extracted_files = os.path.join(data_folder, 'extracted_files')
+
     url = 'https://covid.saude.gov.br/'
     elemento = '/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/div[1]/div[2]/ion-button'
 
     get_element_data(url, xpath=elemento)
-    move_file(downloads_dir, data_folder)
+    move_file(downloads_dir, folder_files_zip)
+    extract_file(folder_extracted_files, folder_files_zip)
